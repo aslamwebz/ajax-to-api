d6791de0befc728c9d36801b0fc077cab762169b
aslamwebz <42026937+aslamwebz@users.noreply.github.com>
2018-12-24 00:17:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+// HTTP Status 
+// 200: onKeyDown
+// 403: forbidden
+// 400: not found
+
+
+function loadData(e){
+	const number = document.querySelector('input[type="number"]').value;
+
+	//Craete an XHR object
+	const xhr = new XMLHttpRequest();
+
+	//open , Asynchronus == true
+	xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+	xhr.onload = function(){
+		if(this.status === 200){
+			const response = JSON.parse(this.responseText);
+			let output = '';
+
+			if(response.type === 'success'){
+				response.value.forEach(function(joke){
+					output += `<li>${joke.joke}</li>`;
+				});
+			}else{
+				output += '<li>Something went wrong</li>';
+			}
+
+			document.querySelector('.jokes').innerHTML = output;
+		}
+	}
+
+	xhr.send();
+
+	e.preventDefault();
+}
